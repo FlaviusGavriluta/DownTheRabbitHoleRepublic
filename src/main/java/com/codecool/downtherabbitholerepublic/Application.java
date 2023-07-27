378ebb7a235d7c85671c62b0f8946aab6b444c97
appuser <appuser@journey2-86bff4fbf7-5gdcq>
2023-07-27 08:44:29 +0000

    starter project

---
+++ src/main/java/com/codecool/downtherabbitholerepublic/Application.java
@@ -0,0 +1,21 @@
+package com.codecool.downtherabbitholerepublic;
+
+public class Application {
+
+    private static final String WORK_DIR = "src/main/resources/";
+
+    public static void main(String[] args)
+    {
+        splitRepublic();
+        splitAlice();
+
+    }
+
+    private static void splitRepublic()
+    {
+    }
+
+    private static void splitAlice()
+    {
+    }
+}
